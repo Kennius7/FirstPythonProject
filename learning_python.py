73423d760f6985638eb3224e7442ab49bc0f12a4
Kenniusboggs <ogbogukenny@yahoo.com>
2022-10-06 17:33:46 +0100

    Initial commit

---
+++ learning_python.py
@@ -0,0 +1,21 @@
+from math import *
+
+test = "My name is Kenny.\nThis is me learning Python"
+my_number = 5
+
+print(test + " and it's cool")
+print(test.upper().isupper())
+print(len(test))
+print(test.index("P"))
+print(str(my_number))
+print(pow(3, 3))
+print(max(3, 8))
+print(round(7.821))
+print(floor(4.5))
+print(ceil(4.5))
+print(sqrt(4.5))
+print(round(sqrt(4.5)))
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + ", aged " + age + "years old!")
+
